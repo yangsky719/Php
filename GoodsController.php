ab87fe130ec6b449d022ecbc944e8aa919b805cc
yoshiyuki <xyz@cjlu.edu.cn>
2019-10-24 14:33:19 +0800

    Review意见
    good job!
    很好的实现了需求
    主要针对代码风格提出一些意见

--- GoodsController.php
+++ GoodsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 
@@ -10,95 +11,85 @@ class GoodsController extends AppController
 {
     public function preOrder()
     {
-      //获取商品名称
-      $goodsname=$this->request->getData('商品');
-      //获取商品订购数量
-      $quantity =$this->request->getData('数量');
-    
-      try{
+        //获取商品名称
+        $goodsname = $this->request->getData('商品');//请考虑在接口传递上使用商品id，以避免重名商品的潜在问题
+        //获取商品订购数量
+        $quantity = $this->request->getData('数量');
+
+        try {
             //
-            $conn =ConnectionManager::get('default');
-            
+            $conn = ConnectionManager::get('default');
+
+            //若无必要，则尽量不用select *
             $results = $conn->execute('select * from traning_goods where name=:name and stock >= :stock',
-                            ['name'=>$goodsname,'stock'=>$quantity])
-                            ->fetchAll('assoc');
-            
+                ['name' => $goodsname, 'stock' => $quantity])
+                ->fetchAll('assoc');
 
-            
+            //return是动词，一般仅用在function上，原则上，变量应全部使用名词形式
+            $result = [];//若php版本大于5.4，则建议数组声明使用字面量形式，更简短
 
-            $returns=array();
-
-            if(count($results)==1)
-            {
+            if (count($results) == 1) {
                 //可以订购
                 //事务开始
-                $Order_Num=null;
+                $OrderNum = null;//驼峰风格不宜与下划线同用
 
                 $conn->begin();
 
                 //扣掉对应商品库存
-                $conn->update('traning_goods', 
-                             ['stock' => ($results[0]['stock']- $quantity) ],
-                             ['name' => $goodsname]);
+                $conn->update('traning_goods',
+                    ['stock' => ($results[0]['stock'] - $quantity)],
+                    ['name' => $goodsname]);
 
                 //生成订单记录
-                $Order_Num=date("YmdHis");
+                $Order_Num = date("YmdHis");
                 $conn->insert('traning_order',
-                             [
-                                    'order_num' => $Order_Num,
-                                    'goods_id' => $results[0]['id'],
-                                    'quantity' => $quantity,
-                                    'created_at' => 0,
-                                    'updated_at' => 0
-                             ]
+                    [
+                        'order_num' => $Order_Num,
+                        'goods_id' => $results[0]['id'],
+                        'quantity' => $quantity,
+                        'created_at' => 0,//字段含义为订单创建时间，应当赋以time()值
+                        'updated_at' => 0,//建议assoc数组多行书写时，最后一行始终加逗号，是一种对git管理更友好的写法
+                    ]
 
                 );
 
                 //提交事务
                 $conn->commit();
 
-                $returns = array(
+                $result = array(
                     'error_code' => 0,
                     'data' => array(
                         'order' => $Order_Num
                     )
                 );
-                
 
 
-                
-            }else{
+            } else {
                 //库存不足
-                $returns = array(
+                $result = array(
                     'error_code' => 1,
-                    'data' => array(
-                        
-                    ),
-                    'msg' =>"库存不足"
+                    'data' => array(),
+                    'msg' => "库存不足"
 
                 );
             }
 
-            $this->set('returns',$returns);
+            $this->set('returns', $result);
+        } catch (Exception $e) {
+            throw new InternalErrorException('错误:' . $e->getMessage());
+        }
+
+
     }
-    catch(Exception $e)
+
+    public function index()
     {
-        throw new InternalErrorException('错误:'.$e->getMessage());
+        $goods = ['商品A' => '商品A', '商品B' => '商品B', '商品C' => '商品C'];
+
+        $this->set('goods', $goods);
+
     }
-
-
-    
-    }
-
-   public function index()
-   {
-       $goods = [ '商品A'=>'商品A','商品B'=>'商品B','商品C'=>'商品C'];
-
-       $this->set('goods',$goods);
-
-   }
 
 }
+
 ?>
-
-
